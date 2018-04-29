25d55c61cc571b45c77559eb9d761c48ee28e650
Thomas Wolfgruber <tomson124@outlook.de>
2018-04-29 22:29:30 +0200

    Fix ArmorPlating Recipes not working - #78

--- src/main/java/tonius/simplyjetpacks/integration/TERecipes.java
+++ src/main/java/tonius/simplyjetpacks/integration/TERecipes.java
@@ -8,40 +8,45 @@ import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 public abstract class TERecipes {
 
+	private static final String input = "input";
+	private static final String input2 = "input2";
+	private static final String output = "output";
+	private static final String output2 = "output2";
+
 	public static void addSmelterRecipe(int energy, ItemStack primaryInput, ItemStack secondaryInput, ItemStack primaryOutput, ItemStack secondaryOutput, int chance) {
 		SimplyJetpacks.logger.info("Registering TE Induction Smelter recipe");
 
 		NBTTagCompound toSend = new NBTTagCompound();
 
 		toSend.setInteger("energy", energy);
-		toSend.setTag("primaryInput", new NBTTagCompound());
-		toSend.setTag("secondaryInput", new NBTTagCompound());
-		toSend.setTag("primaryOutput", new NBTTagCompound());
-		toSend.setTag("secondaryOutput", new NBTTagCompound());
+		toSend.setTag(input, new NBTTagCompound());
+		toSend.setTag(input2, new NBTTagCompound());
+		toSend.setTag(output, new NBTTagCompound());
+		toSend.setTag(output2, new NBTTagCompound());
 
-		primaryInput.writeToNBT(toSend.getCompoundTag("primaryInput"));
-		secondaryInput.writeToNBT(toSend.getCompoundTag("secondaryInput"));
-		primaryOutput.writeToNBT(toSend.getCompoundTag("primaryOutput"));
+		primaryInput.writeToNBT(toSend.getCompoundTag(input));
+		secondaryInput.writeToNBT(toSend.getCompoundTag(input2));
+		primaryOutput.writeToNBT(toSend.getCompoundTag(output));
 		if (secondaryOutput != null) {
-			secondaryOutput.writeToNBT(toSend.getCompoundTag("secondaryOutput"));
+			secondaryOutput.writeToNBT(toSend.getCompoundTag(output2));
 			toSend.setInteger("chance", chance);
 		}
 
 		FMLInterModComms.sendMessage("thermalexpansion", "AddSmelterRecipe", toSend);
 	}
 
-	public static void addTransposerFill(int energy, ItemStack input, ItemStack output, FluidStack fluid, boolean reversible) {
+	public static void addTransposerFill(int energy, ItemStack fluidInput, ItemStack fluidOutput, FluidStack fluid, boolean reversible) {
 		SimplyJetpacks.logger.info("Registering TE Fluid Transposer fill recipe");
 
 		NBTTagCompound toSend = new NBTTagCompound();
 
 		toSend.setInteger("energy", energy);
-		toSend.setTag("input", new NBTTagCompound());
-		toSend.setTag("output", new NBTTagCompound());
+		toSend.setTag(input, new NBTTagCompound());
+		toSend.setTag(output, new NBTTagCompound());
 		toSend.setTag("fluid", new NBTTagCompound());
 
-		input.writeToNBT(toSend.getCompoundTag("input"));
-		output.writeToNBT(toSend.getCompoundTag("output"));
+		fluidInput.writeToNBT(toSend.getCompoundTag(input));
+		fluidOutput.writeToNBT(toSend.getCompoundTag(output));
 		toSend.setBoolean("reversible", reversible);
 		fluid.writeToNBT(toSend.getCompoundTag("fluid"));
 
