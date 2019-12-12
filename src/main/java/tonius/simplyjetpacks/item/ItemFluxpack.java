fe1a1147762eee509b3a64763139ddb79b6ea3f0
Thomas <tomson124@outlook.de>
2019-12-12 22:01:03 +0100

    Temporal fix for ItemFluxpack

--- src/main/java/tonius/simplyjetpacks/item/ItemFluxpack.java
+++ src/main/java/tonius/simplyjetpacks/item/ItemFluxpack.java
@@ -1,65 +1,26 @@
 package tonius.simplyjetpacks.item;
 
-import cofh.redstoneflux.api.IEnergyContainerItem;
-import com.google.common.collect.Multimap;
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.EnumRarity;
-import net.minecraft.item.ItemArmor;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.ISpecialArmor;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.EnumHelper;
-import net.minecraftforge.energy.CapabilityEnergy;
-import net.minecraftforge.energy.IEnergyStorage;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import tonius.simplyjetpacks.SimplyJetpacks;
-import tonius.simplyjetpacks.capability.CapabilityProviderEnergy;
-import tonius.simplyjetpacks.capability.EnergyConversionStorage;
-import tonius.simplyjetpacks.client.model.PackModelType;
-import tonius.simplyjetpacks.client.util.RenderUtils;
-import tonius.simplyjetpacks.config.Config;
-import tonius.simplyjetpacks.setup.FuelType;
-import tonius.simplyjetpacks.setup.ModEnchantments;
-import tonius.simplyjetpacks.setup.ModItems;
-import tonius.simplyjetpacks.util.EquipmentSlotHelper;
-import tonius.simplyjetpacks.util.ItemHelper;
 import tonius.simplyjetpacks.util.NBTHelper;
 import tonius.simplyjetpacks.util.SJStringHelper;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
-public class ItemFluxpack extends ItemPack implements ISpecialArmor, IEnergyContainerItem, IHUDInfoProvider {
+public class ItemFluxpack extends ItemPack implements IHUDInfoProvider {
 
 	public static final String TAG_ENERGY = "Energy";
-	public static final String TAG_ON = "PackOn";
 
-	public ItemFluxpack(String name) {
-		super(EnumHelper.addArmorMaterial("JETPACK_SJ", "jetpack", 0, new int[]{0, 0, 0, 0}, 0, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0), 2, EntityEquipmentSlot.CHEST, name);
+	public ItemFluxpack(Item.Properties properties) {
+		super(ArmorMaterial.IRON, EquipmentSlotType.CHEST, properties);
 	}
 
-	@Override
+	/*@Override
 	public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5) {
 	}
 
@@ -101,5 +62,5 @@ public class ItemFluxpack extends ItemPack implements ISpecialArmor, IEnergyCont
 	@SideOnly(Side.CLIENT)
 	public String getHUDStatesInfo(ItemStack stack) {
 		return SJStringHelper.getHUDStateText(this.isOn(stack), null, null);
-	}
+	}*/
 }
