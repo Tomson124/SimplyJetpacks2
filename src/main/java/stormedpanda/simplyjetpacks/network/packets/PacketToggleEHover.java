8d3106d6d9b8db0aabbc7ac97059085e5ab86c86
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-08-19 13:47:23 +0100

    optimized imports

--- src/main/java/stormedpanda/simplyjetpacks/network/packets/PacketToggleEHover.java
+++ src/main/java/stormedpanda/simplyjetpacks/network/packets/PacketToggleEHover.java
@@ -1,6 +1,5 @@
 package stormedpanda.simplyjetpacks.network.packets;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
