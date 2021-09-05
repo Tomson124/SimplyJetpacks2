94cf6c463cbef7001660936b1eb4b2fff9928da9
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-05 17:53:35 +0100

    new things - added thermal jetpack and thruster recipes - added data gen recipes for some items

--- src/main/java/stormedpanda/simplyjetpacks/datagen/SJRecipeProvider.java
+++ src/main/java/stormedpanda/simplyjetpacks/datagen/SJRecipeProvider.java
@@ -1,10 +1,11 @@
 package stormedpanda.simplyjetpacks.datagen;
 
-import net.minecraft.data.CustomRecipeBuilder;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
+import com.google.gson.JsonObject;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.data.*;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.Tags;
@@ -23,7 +24,98 @@ public class SJRecipeProvider extends RecipeProvider implements IConditionBuilde
 
     @Override
     protected void buildShapelessRecipes(@Nonnull Consumer<IFinishedRecipe> consumer) {
-        CustomRecipeBuilder.special(RegistryHandler.JETPACK_SPECIAL_RECIPE.get()).save(consumer, new ResourceLocation(SimplyJetpacks.MODID, "jetpack_special_recipe").toString());
+        CustomRecipeBuilder.special(RegistryHandler.JETPACK_SPECIAL_RECIPE.get()).save(consumer, savePath("jetpack_special_recipe"));
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.LEATHER_STRAP.get())
+                .pattern("LIL")
+                .pattern("LIL")
+                .define('I', forgeTag("ingots/iron"))
+                .define('L', forgeTag("leather"))
+                .unlockedBy("has_item", InventoryChangeTrigger.Instance.hasItems(Items.LEATHER))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.JETPACK_POTATO.get())
+                .pattern("S S")
+                .pattern("GPG")
+                .pattern("R R")
+                .define('S', forgeTag("string"))
+                .define('G', forgeTag("nuggets/gold"))
+                .define('P', forgeTag("crops/potato"))
+                .define('R', forgeTag("dusts/redstone"))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PILOT_GOGGLES_GOLD.get())
+                .pattern(" S ")
+                .pattern("GIG")
+                .define('S', RegistryHandler.LEATHER_STRAP.get())
+                .define('G', forgeTag("glass_panes"))
+                .define('I', forgeTag("ingots/gold"))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PILOT_GOGGLES_IRON.get())
+                .pattern(" S ")
+                .pattern("GIG")
+                .define('S', RegistryHandler.LEATHER_STRAP.get())
+                .define('G', forgeTag("glass_panes"))
+                .define('I', forgeTag("ingots/iron"))
+                .save(consumer);
+
+        ShapelessRecipeBuilder.shapeless(RegistryHandler.PARTICLE_BLEND.get(), 2)
+                .requires(Items.COAL)
+                .requires(Items.CLAY_BALL)
+                .requires(Items.GUNPOWDER)
+                .requires(Items.BONE_MEAL)
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PARTICLE_NONE.get())
+                .pattern(" T ")
+                .pattern("TPT")
+                .pattern(" T ")
+                .define('T', Items.GLASS)
+                .define('P', RegistryHandler.PARTICLE_BLEND.get())
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PARTICLE_FLAME.get())
+                .pattern(" T ")
+                .pattern("TPT")
+                .pattern(" T ")
+                .define('T', Items.TORCH)
+                .define('P', RegistryHandler.PARTICLE_BLEND.get())
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PARTICLE_SMOKE.get())
+                .pattern(" T ")
+                .pattern("TPT")
+                .pattern(" T ")
+                .define('T', Items.COAL)
+                .define('P', RegistryHandler.PARTICLE_BLEND.get())
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PARTICLE_RAINBOW.get())
+                .pattern(" R ")
+                .pattern(" P ")
+                .pattern("G B")
+                .define('R', forgeTag("dyes/red"))
+                .define('G', forgeTag("dyes/green"))
+                .define('B', forgeTag("dyes/blue"))
+                .define('P', RegistryHandler.PARTICLE_BLEND.get())
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PARTICLE_SOUL.get())
+                .pattern(" T ")
+                .pattern("TPT")
+                .pattern(" T ")
+                .define('T', Items.SOUL_TORCH)
+                .define('P', RegistryHandler.PARTICLE_BLEND.get())
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RegistryHandler.PARTICLE_SNOW.get())
+                .pattern(" T ")
+                .pattern("TPT")
+                .pattern(" T ")
+                .define('T', Items.SNOWBALL)
+                .define('P', RegistryHandler.PARTICLE_BLEND.get())
+                .save(consumer);
     }
 
     private static ResourceLocation modId(String path) {
@@ -32,5 +124,15 @@ public class SJRecipeProvider extends RecipeProvider implements IConditionBuilde
 
     private static Tags.IOptionalNamedTag<Item> forgeTag(String name) {
         return ItemTags.createOptional(new ResourceLocation("forge", name));
+    }
+
+    private static Ingredient fromJson(String name) {
+        JsonObject object = new JsonObject();
+        object.addProperty("item", name);
+        return Ingredient.fromJson(object);
+    }
+
+    private static String savePath(String path) {
+        return new ResourceLocation(SimplyJetpacks.MODID, path).toString();
     }
 }
