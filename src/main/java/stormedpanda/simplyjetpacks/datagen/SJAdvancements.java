2dab7f74b0bc6b0884765b8a3eb82002128a21f8
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-29 20:43:45 +0100

    new data gen stuff

--- src/main/java/stormedpanda/simplyjetpacks/datagen/SJAdvancements.java
+++ src/main/java/stormedpanda/simplyjetpacks/datagen/SJAdvancements.java
@@ -38,6 +38,33 @@ public class SJAdvancements implements Consumer<Consumer<Advancement>> {
         Advancement vanilla3_armored = jetpackAdvancement(consumer, vanilla3, RegistryHandler.JETPACK_VANILLA3_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
         Advancement vanilla4 = jetpackAdvancement(consumer, vanilla3, RegistryHandler.JETPACK_VANILLA4.get(), "simplyjetpacks", FrameType.CHALLENGE);
         Advancement vanilla4_armored = jetpackAdvancement(consumer, vanilla4, RegistryHandler.JETPACK_VANILLA4_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        // Mekanism:
+        Advancement mek1 = jetpackAdvancement(consumer, root, RegistryHandler.JETPACK_MEK1.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement mek1_armored = jetpackAdvancement(consumer, mek1, RegistryHandler.JETPACK_MEK1_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement mek2 = jetpackAdvancement(consumer, mek1, RegistryHandler.JETPACK_MEK2.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement mek2_armored = jetpackAdvancement(consumer, mek2, RegistryHandler.JETPACK_MEK2_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement mek3 = jetpackAdvancement(consumer, mek2, RegistryHandler.JETPACK_MEK3.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement mek3_armored = jetpackAdvancement(consumer, mek3, RegistryHandler.JETPACK_MEK3_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement mek4 = jetpackAdvancement(consumer, mek3, RegistryHandler.JETPACK_MEK4.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement mek4_armored = jetpackAdvancement(consumer, mek4, RegistryHandler.JETPACK_MEK4_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        // Immersive Engineering:
+        Advancement ie1 = jetpackAdvancement(consumer, root, RegistryHandler.JETPACK_IE1.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement ie1_armored = jetpackAdvancement(consumer, ie1, RegistryHandler.JETPACK_IE1_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement ie2 = jetpackAdvancement(consumer, ie1, RegistryHandler.JETPACK_IE2.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement ie2_armored = jetpackAdvancement(consumer, ie2, RegistryHandler.JETPACK_IE2_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement ie3 = jetpackAdvancement(consumer, ie2, RegistryHandler.JETPACK_IE3.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement ie3_armored = jetpackAdvancement(consumer, ie3, RegistryHandler.JETPACK_IE3_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        // Thermal:
+        Advancement te1 = jetpackAdvancement(consumer, root, RegistryHandler.JETPACK_TE1.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te1_armored = jetpackAdvancement(consumer, te1, RegistryHandler.JETPACK_TE1_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te2 = jetpackAdvancement(consumer, te1, RegistryHandler.JETPACK_TE2.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te2_armored = jetpackAdvancement(consumer, te2, RegistryHandler.JETPACK_TE2_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te3 = jetpackAdvancement(consumer, te2, RegistryHandler.JETPACK_TE3.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te3_armored = jetpackAdvancement(consumer, te3, RegistryHandler.JETPACK_TE3_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te4 = jetpackAdvancement(consumer, te3, RegistryHandler.JETPACK_TE4.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te4_armored = jetpackAdvancement(consumer, te4, RegistryHandler.JETPACK_TE4_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te5 = jetpackAdvancement(consumer, te3, RegistryHandler.JETPACK_TE5.get(), "simplyjetpacks", FrameType.CHALLENGE);
+        Advancement te5_armored = jetpackAdvancement(consumer, te4, RegistryHandler.JETPACK_TE5_ARMORED.get(), "simplyjetpacks", FrameType.CHALLENGE);
     }
 
     private Advancement jetpackAdvancement(Consumer<Advancement> consumer, Advancement parent, IItemProvider item, String path, FrameType frame) {
